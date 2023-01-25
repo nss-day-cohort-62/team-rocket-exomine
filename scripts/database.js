397454e6128aad6468d09b273f39b29e5e906368
Vision Filler <visionfiller@gmail.com>
2023-01-25 10:59:27 -0600

    clone this

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,24 @@
-const database = {
+
+    const database = {
+        colonies: [
+            {id: 1, name: 'Halo'},
+            {id: 2, name: 'Earth'},
+            {id: 3, name: 'Mars'},
+            {id: 4, name: 'Pluto'},
+        ],
+        governors: [
+            {id: 1, name: 'Jackson Henderson', status: 'Active', colonyId: 1},
+            {id: 2, name: 'Hannah Collins', status: 'Active', colonyId: 3},
+            {id: 3, name: 'Vision Filler', status: "Inactive", colonyId: 4},
+            {id: 4, name: 'Dolly Parton', status: 'Active', colonyId: 2},
+            {id: 5, name: 'Master Chief', status: 'Active', colonyId: 1},
+            {id: 6, name: 'Seargent Johnson', status: 'Inactive', colonyId: 1},
+            {id: 7, name: 'Triston Blaney', status: 'Active', colonyId: 3},
+            {id: 8, name: 'Ada Collins', status: 'Active', colonyId: 3},
+            {id: 9, name: 'David Allen Coe', status: 'Inactive', colonyId: 2},
+            {id: 10, name: 'Taylor Swift', status: 'Active', colonyId: 2},
+            {id: 11, name: 'Wesley Hughes', status: 'Active', colonyId: 4},
+            {id: 12, name: 'Poppy Filler Hughes'
     transientState: {}
 }
 
